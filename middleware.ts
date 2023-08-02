4170c62fc16cdcdf39eb19bddbe0352ab0369303
omkarkokil <nsjustice21@gmail.com>
2023-08-02 22:20:34 +0530

    temp -middleware change

--- middleware.ts
+++ middleware.ts
@@ -7,5 +7,8 @@ export default withAuth({
 });
 
 export const config = {
-  matcher: ["/users/:path*", "/conversations/:path*"],
+  matcher: [
+    "/conversations/:path*",
+    "/users/:path*",
+  ]
 };
